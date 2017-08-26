5d1ae9de61e2ccb71b20c732edb15726b9b547ae
mahdi hajian <mahdi.haj.1375@gmail.com>
2017-08-26 06:27:10 +0430

    for ver2 part-1

--- tamrin.js
+++ tamrin.js
@@ -1,51 +1,24 @@
 $(document).ready(function () {
-    $("#btnHide").click(function () {
-        $("#google").hide();
+    $("#btnAnimate1").click(function () {
+        $("#blueSpace").animate({
+            width: "600px",
+        }, 1000).animate({
+            height: "500px"
+        }, 890);
+
+        $("#fontofblueSpace").animate({
+            left: "220px"
+        }, 1200).animate({
+            top: "205px"
+        }, 1000)
     });
 
-    $("#btnShow").click(function () {
-        $("#google").show();
-    });
-
-    $("#btnToggle").click(function () {
-        $("#google").toggle();
-    });
-
-    $("#btnFadein").click(function () {
-        $("#google").fadeIn();
-    });
-
-    $("#btnFadeout").click(function () {
-        $("#google").fadeOut();
-    });
-
-    $("#btnFadetoggle").click(function () {
-        $("#google").fadeToggle();
-    });
-
-    $("#btnsLideUp").click(function () {
-        $("#google").slideUp();
-    });
-
-    $("#btnsLideDown").click(function () {
-        $("#google").slideDown();
-    });
-
-    $("#btnsLideToggle").click(function () {
-        $("#google").slideToggle();
-    });
-
-    //                $("#btnsLideToggle").click(function () {
-    //        $("#google").slideToggle();
-    //    });
-
-    $("#Animate").click(function () {
-        $("#google").animate({
-            left: "300px",
-            opacity: "0.5px",
-            height: "50px",
-            width: "150"
-        });
+    $("#btnStop").click(function () {
+        $("#blueSpace").stop();
+        $("#blueSpace").stop();
+        $("#fontofblueSpace").stop();
+        $("#fontofblueSpace").stop();
     });
 
 });
+
