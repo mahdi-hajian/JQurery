02b800083dd8937e5ffb058504ace422546ce261
mahdi hajian <mahdi.haj.1375@gmail.com>
2017-08-26 09:29:55 +0430

    pack3

--- tamrin.js
+++ tamrin.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $("#btnval").click(function () {
         var name = $("#txtval").val();
         $("#txtresult").val(name);
+
     });
 
     $("#changehref").click(function () {
@@ -13,5 +14,29 @@ $(document).ready(function () {
         var push = $("#txtbtnchange").val();
         $("#btnchange").html(push);
     });
+
+    $("#btnafter").click(function () {
+        var dif = $("#txtdifrent").val()
+        $("#btnafter").after(dif);
+    });
+
+    $("#btnbefore").click(function () {
+
+        var dif = $("#txtdifrent").val()
+        $("#btnbefore").before(dif);
+
+    })
+    $("#btnappend").click(function () {
+
+        var dif = $("#txtdifrent").val()
+        $("#btnappend").append(dif);
+
+    })
+    $("#btnprepend").click(function () {
+
+        var dif = $("#txtdifrent").val()
+        $("#btnprepend").prepend(dif);
+
+    })
 
 });
