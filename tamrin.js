6512cf96d204ce11b49e89575293e95d2a181ac7
mahdi hajian <mahdi.haj.1375@gmail.com>
2017-08-27 14:20:59 +0430

    ready for ver-5

--- tamrin.js
+++
@@ -1,9 +0,0 @@
-$("#slider-container img").hide();
-$("#slider-container :first-child").show();
-function showNext(){
-   $("#slider-container :first-child").fadeOut(2000).next("img").fadeIn().end().appendTo("#slider-container");
-}
-function showPrev(){
-    $("#slider-container :last-child").fadeOut(2000).prev("img").fadeIn().end().appendTo("#slider-container");
-}
-setInterval(showNext,5000);
